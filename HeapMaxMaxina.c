a96d8d926acf2bd5191678db9dc8ea735f8415d0
Ana-Maria Pamfil <ana.pamfil@hotmail.com>
2025-05-20 18:21:23 +0300

    Heap Max

--- HeapMaxMaxina.c
+++ HeapMaxMaxina.c
@@ -20,6 +20,25 @@ int nrMasini;
 };
 typedef struct Heap Heap;
 
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+	m1.serie = *strtok(NULL, sep);
 
+	return m1;
+}
 	
 
