08d9e1eb3b43cce9fa90950c453fec8b4d9aa9ca
Ana-Maria Pamfil <ana.pamfil@hotmail.com>
2025-05-21 17:16:47 +0300

    Exersare heap

--- ExersareHeap1.c
+++ ExersareHeap1.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS_
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,8 +29,8 @@ Ruj citireRujDinFisier(FILE* file) {
 	r.pret = atof(strtok(NULL, sep));
 
 	aux = strtok(NULL, sep);
-	r.brand = malloc(strlen_s(aux + 1));
-	strcpy_s(r.brand, strlen_s(aux + 1), aux);
+	r.brand = malloc(strlen(aux)+1);
+	strcpy_s(r.brand, strlen(aux)+1, aux);
 	return r;
 }
 
@@ -39,4 +39,102 @@ void afisareRuj(Ruj r) {
 	printf("Pret:%.2f\n", r.pret);
 	printf("Brandul rujului:%s\n", r.brand);
 }
+
+Heap initializareHeap(int capacitate) {
+	Heap h;
+	h.lungime = capacitate;
+	h.nrElemente = 0;
+	h.vector = (Ruj*)malloc(sizeof(Ruj) * capacitate);
+
+	return h;
+
+}
+
+void filtrareHeap(Heap h, int poz) {
+	int st = 2 * poz + 1;
+	int dr = 2 * poz + 2;
+	int max = poz;
+
+	if (st<h.nrElemente && h.vector[st].pret>h.vector[max].pret)
+		max = st;
+	if (dr<h.nrElemente && h.vector[dr].pret>h.vector[max].pret)
+		max = dr;
+	if (max != poz) {
+		Ruj aux = h.vector[max];
+		h.vector[max] = h.vector[poz];
+		h.vector[poz] = aux;
+
+		if (max >= (h.nrElemente - 2)/2) {
+			filtrareHeap(h, max);
+		}
+	}
+}
+
+Heap citireHeapDinFisier(const char* numeFisier) {
+	FILE* file = fopen(numeFisier, "r");
+	Heap h = initializareHeap(10);
+	while (!feof(file)) {
+		h.vector[h.nrElemente++] = citireRujDinFisier(file);
+	}
+	fclose(file);
+	for (int i = (h.nrElemente - 2) / 2; i >= 0; i--) {
+		filtrareHeap(h, i);
+	}
+
+	return h;
+}
+
+void afisareHeap(Heap h) {
+	for (int i = 0; i < h.nrElemente; i++) {
+		afisareRuj(h.vector[i]);
+	}
+
+}
+
+Ruj extragereRuj(Heap* h) {
+	if (h->nrElemente > 0) {
+		Ruj r = h->vector[0];
+		h->vector[0] = h->vector[h->nrElemente - 1];
+		h->vector[h->nrElemente - 1] = r;
+	
+	h->nrElemente--;
+
+	for (int i = (h->nrElemente - 2) / 2; i >= 0; i--) {
+		filtrareHeap(*h, i);
+	}
+
+	return r;
+	}
+}
+
+void dezalocareHeap(Heap* h) {
+	for (int i = 0; i < h->lungime; i++) {
+		free(h->vector[i].brand);
+	}
+	free(h->vector);
+	h->vector = NULL;
+	h->lungime = h->nrElemente = 0;
+}
+
+
+int main() {
+	Heap heap = citireHeapDinFisier("DateRuj.txt");
+	afisareHeap(heap);
+	printf("elemente extrase: \n\n");
+	for (int i = 0; i < 10; i++) { //avem 10 rujuri in fisierul cu date
+		afisareRuj(extragereRuj(&heap));
+	}
+	dezalocareHeap(&heap);
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
 
