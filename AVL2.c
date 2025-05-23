3ee04380da0c26ff112764a7d26295c9cc28ecd8
Ana-Maria Pamfil <ana.pamfil@hotmail.com>
2025-05-23 15:42:04 +0300

    Exersare AVL 2

--- AVL2.c
+++ AVL2.c
@@ -46,21 +46,105 @@ int gradEchi(Nod* rad) {
 }
 
 //Rotire stand
-
+Nod* rotireStanga(Nod* rad) {
+	if (rad) {
+		Nod* aux = rad->dr;
+		rad->dr = aux->st;
+		aux->st = rad;
+		return aux;
+	}
+	return rad;
+}
 
 
 //Rotire dreapta
 
+Nod* rotireDreapta(Nod* rad) {
+	if (rad) {
+		Nod* aux = rad->st;
+		rad->st = aux->dr;
+		aux->dr = rad;
+		return aux;
+	}
+	return rad;
+}
+
+//Inserare
+
+Nod* inserareAVL(Nod* rad, Lampa l) {
+	if (rad) {
+		if (l.id < rad->info.id) rad->st = inserareAVL(rad->st, l);
+		else rad->dr = inserareAVL(rad->dr, l);
+		int ge = gradEchi(rad);
+		if (ge == -2) { //dezechi de dreapta
+			if (gradEchi(rad->dr) > 0)
+				rad->dr = rotireDreapta(rad->dr);
+			rad = rotireStanga(rad);
+		}
+		if (ge == 2) {//dezechi de stanga
+			if (gradEchi(rad->st) < 0) rad->st = rotireStanga(rad->st);
+			rad = rotireDreapta(rad);
+		}
+	
+	return rad;
+	}
+	else {
+		Nod* nou = (Nod*)malloc(sizeof(Nod));
+		nou->info = l;
+		nou->st = nou->dr = NULL;
+		return nou;
+	}
+}
+
+void afisareLampa(Lampa l) {
+	printf("Lampa cu id-ul: %d , este facuta din materialul: %s , are pretul: %.2f \n", l.id, l.material, l.pret);
+}
+
+void afisareInOrdine(Nod* rad) {
+	if (rad) {
+		afisareInOrdine(rad->st);
+		afisareLampa(rad->info);
+		afisareInOrdine(rad->dr);
+	}
+}
 
 
+Nod* citireDinFisier(const char* numeFisier, int n) {
+	FILE* f = fopen(numeFisier, "r");
+
+	Nod* rad = NULL;
+	for (int i = 0; i < n; i++) {
+		int id;
+		char material[100];
+		float pret;
+		fscanf(f, "%d,%99[^,],%f", &id, material, &pret);
+		rad = inserareAVL(rad, crearelampa(id, material, pret));
+	}
+
+	fclose(f);
+	return rad;
+}
 
 
+void dezalocare(Nod* rad) {
+	if (rad) {
+		dezalocare(rad->st);
+		dezalocare(rad->dr);
+		free(rad->info.material);
+		free(rad);
+	}
+}
 
 
 
 
 int main() {
-
+	Nod* rad = citireDinFisier("dateLampa.txt", 10);
+	if (rad) {
+		printf("Arbore AVL in inordine:\n");
+		afisareInOrdine(rad);
+		dezalocare(rad);
+	}
 
 	return 0;
 }
